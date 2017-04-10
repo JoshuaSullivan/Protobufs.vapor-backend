10d3218676b061009f038269c88b6e37e53fa253
Joshua Sullivan <jsulliva@nerdery.com>
2017-04-10 14:57:07 -0500

    Got the first version of the server running.

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,31 @@
 import Vapor
+import HTTP
+import Foundation
+import SwiftProtobuf
+
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.post("/proto") {
+    request in
+    guard let bytes = request.body.bytes else {
+        throw Abort.badRequest
+    }
+    let data = Data(bytes: bytes)
+    do {
+        let msgIn = try SimpleMessage(serializedData: data)
+        print("Got message: \(msgIn.content)")
+        let outString = String(msgIn.content.characters.reversed())
+        var msgOut = SimpleMessage()
+        msgOut.content = outString
+        let outData = try msgOut.serializedData()
+        var response = Response(body: try msgOut.serializedData())
+        response.headers["content-type"] = "application/octet-stream"
+        return response
+    } catch {
+        NSLog("Failed to process message: \(error.localizedDescription)")
+        throw Abort.serverError
+    }
 }
-
-drop.resource("posts", PostController())
 
 drop.run()
